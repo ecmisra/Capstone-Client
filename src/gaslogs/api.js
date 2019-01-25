b0fd8fd03ea09c4d210f8ccfbe114543a3088d71
ecmisra <eshanmisra@gmail.com>
2019-01-25 09:24:20 -0500

    Commenting out gaslogs/api.js
    Comment out entire file, too scared to delete it now because of paranoid
    fear that it would break my site (even though I know it wouldn't because
    I never use it anywhere, I'm playing better safe than sorry)

--- src/gaslogs/api.js
+++ src/gaslogs/api.js
@@ -1,52 +1,60 @@
-import apiUrl from '../apiConfig'
 
-export const handleErrors = res => {
-  if (res.ok) {
-    return res
-  } else  {
-    throw new Error('Recieved status in 400 or 500 range.')
-  }
-}
 
-export const createGasLog = credentials => {
-  return fetch(apiUrl + '/gas_logs', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization':`Token token=${user.token}`
-    },
-  })
-}
+// THIS FILE WAS NOT USED, BUT I DIDN'T WANT TO ERASE BEFORE PRESENTATION
+// AND RISK BREAKING SOMETHING. WILL DELETE AFTER GRADUATION
 
-export const signIn = credentials => {
-  return fetch(apiUrl + '/sign-in', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-    })
-  })
-}
 
-export const signOut = user => {
-  return fetch(apiUrl + '/sign-out', {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization':`Token token=${user.token}`
-    }
-  })
-}
 
-export const changePassword = (passwords, user) => {
-  return fetch(apiUrl + '/change-password', {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization':`Token token=${user.token}`
-    },
-    body: JSON.stringify({
-    })
-  })
-}
+
+// import apiUrl from '../apiConfig'
+//
+// export const handleErrors = res => {
+//   if (res.ok) {
+//     return res
+//   } else  {
+//     throw new Error('Recieved status in 400 or 500 range.')
+//   }
+// }
+//
+// export const createGasLog = credentials => {
+//   return fetch(apiUrl + '/gas_logs', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization':`Token token=${user.token}`
+//     },
+//   })
+// }
+//
+// export const signIn = credentials => {
+//   return fetch(apiUrl + '/sign-in', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//     })
+//   })
+// }
+//
+// export const signOut = user => {
+//   return fetch(apiUrl + '/sign-out', {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization':`Token token=${user.token}`
+//     }
+//   })
+// }
+//
+// export const changePassword = (passwords, user) => {
+//   return fetch(apiUrl + '/change-password', {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization':`Token token=${user.token}`
+//     },
+//     body: JSON.stringify({
+//     })
+//   })
+// }
